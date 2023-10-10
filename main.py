9613f818a0773a08f92c33dc85ab281fea44cbfa
Arturo Mendivil <arturoo1516@gmail.com>
2023-10-10 13:01:30 -0700

    docker and k8s files

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import gradio as gr
 
+title = "Welcome to Gradio!"
+description = "This is a demo of a Gradio app."
+ref = "Find the whole code [here](https://github.com/artmen1516/gradio-hello-world)."
+
 def greet(name):
     return "Hello " + name + "!"
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+demo = gr.Interface(fn=greet, inputs="text", outputs="text", title=title, description=description, article=ref)
     
-demo.launch()
+demo.launch(server_name="0.0.0.0", server_port=8080)
